0a84c5eaf9456eb5b4f4945b3e56943a354d56e4
Subhrakamal Das <jyb2084.usa@gmail.com>
2021-11-02 20:23:49 +0530

    Using cin.getline( )1 in C++

---
+++ 2_Basic Sorting Techniques/7_Using cin.getline() in C++ .cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+
+
+
+
+    char paragraph[1000];
+
+    cin.getline(paragraph, 1000, '#');
+
+    cout << paragraph << endl;
+
+    return 0;
+
+}
