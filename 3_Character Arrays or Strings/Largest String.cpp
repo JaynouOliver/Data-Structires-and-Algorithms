e54cd8ff20ef590bdd8bdc880c58d26430bf0d22
Subhrakamal Das <jyb2084.usa@gmail.com>
2022-01-05 22:39:46 +0530

    Character Arrays / String

--- 3_Character Arrays or Strings/Largest String.cpp
+++ 3_Character Arrays or Strings/Largest String.cpp
@@ -1,24 +1,30 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
+
+// Problem : Read N Strings and print the largest string. Each string can have upto 1000 characters.
 
 int main(){
 
-    int n;
-    cin >> n;
+	int n;
+	cin>>n;
 
-    cin.get();
+	cin.get();
 
-    char sentence[1000];
-    char largest[1000];
+	char sentence[1000];
+	char largest[1000];
 
-    int largest_len = 0;
+	int largest_len = 0;
 
-    while(n--){
-        cin.getline(sentence,1000);
-        // cout << sentence << endl;
-        int len = strlen(sentence);
-        
-    }
+	while(n--){
+		cin.getline(sentence,1000);
+		//cout<< sentence <<endl;
+		int len = strlen(sentence);
+		if(len > largest_len){
+			largest_len = len;
+			strcpy(largest,sentence);
+		}
+	}
 
-    return 0;
+	cout<<"Largest sentence is "<<largest <<endl;
+	return 0;
 }
